96430c822d89db28625a33224fbc2b7e45144e29
Raushan <raushanmandallkr@gmail.com>
2023-10-30 06:09:01 -0700

    Inserting elements into an array

--- 2_arrayInsertion.c
+++ 2_arrayInsertion.c
@@ -2,25 +2,20 @@
 #include <stdio.h>
 #include <stdio.h>
 
+void printingElements(int, int[]);
 void arrayInsertion(int[], int, int, int);
 int main()
 {
     int array[10] = {1, 2, 3, 4, 5, 6};
     int size = 6, index = 1, insertElement = 7;
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d\t", array[i]);
-    }
+    printingElements(size, array);
     printf("\n\n");
     arrayInsertion(array, size, index, insertElement);
     size += 1;
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d\t", array[i]);
-    }
+    printingElements(size, array);
     return 0;
-}
-
+} // } main
+// inserting elements into an arrayḤ
 void arrayInsertion(int arr[], int size, int index, int insertElement)
 {
     for (int i = size - 1; i >= index; i--) // tab tak chalega jab tak hum apne us index[1] pe na phunch jaaye, i = 5, 5-1 = 4,3,2,1..
@@ -29,6 +24,14 @@ void arrayInsertion(int arr[], int size, int index, int insertElement)
     }
     // loop khatam hone ke baad ab humara array[1] khali ho chuka hai, ab hum usme apne element ko daal sakte hain.
     arr[index] = insertElement;
+}
+// just prints the elements of array
+void printingElements(int size, int array[])
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", array[i]);
+    }
 }
 /*
 for-loop ke andar [i] ki value hum array ke indices ke hisaab se denge, kyunki array ka index 0 se start hota hai,
@@ -39,7 +42,7 @@ hum index[5] ke elements ko index [6] pe bhej denge
 hum index[4] ke elements ko index [5] pe bhej denge
 hum index[3] ke elements ko index [4] pe bhej denge
 hum index[2] ke elements ko index [3] pe bhej denge
-hum index[1] ke elements ko index [2] pe bhej denge 
+hum index[1] ke elements ko index [2] pe bhej denge
 // arrayInsertion ka loop yahin aakar khatam ho jayega, kyunki i = 1 ho chuka hai, aur index = 1 tha, aur humne apne loop me
 -> yahi condition lagaya tha ki, loop tab tak chale jab tak ki humare 'i' ki value 1 ya usse bara rhe, yani ghatkar jab 1 hoga
 -> toh humara condition false ho jayega, kyunki 1-1 = 0, aur '0' '1' se bara nhi hai, toh yahin tak loop chalega humara
