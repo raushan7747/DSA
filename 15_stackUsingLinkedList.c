556884c3d29ad1709482353632ef9d732014479c
Raushan <raushanmandallkr@gmail.com>
2023-11-29 22:57:08 -0800

    STACK - Linked listlllllllllllll

--- 15_stackUsingLinkedList.c
+++ 15_stackUsingLinkedList.c
@@ -1,3 +1,22 @@
+//  STACK implementation is done by opposite side of Linked list
+/*
+ * When we try to push our first element, woh sabse neeche jayega, such as we're creating 3-elements STACK, then
+ * first-element sabse neeche jakar, 3rd element ho jayega, aur humara 2nd element, 3rd ke upar rhega, usitarah se
+ * 3rd element sabse aakhri me jayega, lekin woh STACK ke sabse upar rhega. Toh jab hum STACK ke elements ko access karna
+ * chahte hain, to hum apne 3rd element ko 2nd element ke saath link karenge, isse jab hum apne 3rd(or, last) element ko
+ * acess karenge, to hum 3rd linked-list ke pointer-field (jisme 2nd element ka Address hai) ki help se
+ * 2nd element ko bhi acess kar payenge, aur phir 2nd element ke paas usi tarah se humare 1st linked-list ka Address hoga,
+ * aur hum jab apne 1st element ke paas pahunch jayenge, to uske baad to koi elements hai nhi humare STACK me to isiliye
+ * hum, 1st element ke pointer-field me NULL put kar denge, jisse ki humara STACK khatam ho sake.
+ * THEREFORE, 1st-element ke paas NULL ka address hona chahiye, kyunki 1st-element sabse aakhri me access kiya jayega, aur hum
+ * chahenge ki aakhri element ke data ke baad humara STACK khatam ho jaaye, isliye 1st-element me hi pehle hum NULL put karenge,
+ * aur uske upar jitne bhi elements hum push karenge, use aakhri element ka address bhi pass karenge, taki jab hum STACK ke upar se
+ * elements push kar rhen honge, to new element upar honge, aur uske andar jo pehle se hain hum use access karne ke liye, naye
+ * element ko pehle waale element ka address pass kar denge.
+ * For Example- STACK me 1 element hai, hum naya element push karenge to, 2nd element ko hum 1st element ka address pass kar denge,
+ * aur 1st element ke pass NULL ka address rhege, jo ki sabse aakhri me acess kiya jayega, aur 1st element ke paas aakar hi humara
+ * STACK-end hoga. According to LIFO- lAST IN FIRST OUT, and FILO- FIRST IN LAST OUT
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -108,6 +127,7 @@ int main()
     {
         printf("Value at position %d is : %d\n", i, peek(i));
     }
+<<<<<<< HEAD
     printf("\nAdress before pop: %u\n", top);
     printf("The top-most element is the %d\n\n", stackTopValue(top));
     // Pop operation must be after Peek operation, that's why, when we popped-out our element then (top) will addressed to the next Node.
@@ -118,6 +138,8 @@ int main()
     int elementPopped = pop(top);
     printf("Popped element is: %d\n", elementPopped);
     printf("\nAdress after pop: %u", top);
+=======
+>>>>>>> 9ab37ca81df40eaa40dc33f09c7794459b13ab51
     return 0;
 }
 /*
